597c513bd684248ccc6254bd6ca919364ee11d8a
Deepak3015 <d03748651@gmail.com>
2025-08-12 17:18:39 +0530

    Pattern in DSA

--- DSA_12_Aug.cpp
+++ DSA_12_Aug.cpp
@@ -16,23 +16,44 @@
 //     }
 
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+//     {
+//         int n;
+//         cin >> n;
+//         int i = 1;
+//         while(i<=n)
+//             {
+//                 int j = 1;
+//                 while (j<=n)
+//                     {
+//                         cout<<"*";
+//                         j++;
+//                     }
+//                     cout<<endl;
+//                     i++;
+//             }
+//     }
+
 #include <iostream>
 using namespace std;
 
 int main()
     {
         int n;
-        cin >> n;
-        int i = 1;
-        while(i<=n)
+        cin>>n;
+        int i =1;
+        while (i<=n)
             {
                 int j = 1;
                 while (j<=n)
                     {
-                        cout<<"*";
+                        cout<<n-j+1;
                         j++;
                     }
-                    cout<<endl;
-                    i++;
+                cout<<endl;
+                i++;
             }
     }
