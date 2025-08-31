476a47d8b93f1b59b87c2076617d7151fc4b7bf7
Deepak3015 <d03748651@gmail.com>
2025-08-31 16:47:34 +0530

    Second one

--- DSA.cc
+++ DSA.cc
@@ -1,15 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a = 2;
-    int b = a+1;
-    if((a=3)==b){
-        cout<<a;
-    }
-    else{
-        cout<<a+1;
-    }
-    cin.get(); 
+// int main(){
+//     int a = 2;
+//     int b = a+1;
+//     if((a=3)==b){
+//         cout<<a;
+//     }
+//     else{
+//         cout<<a+1;
+//     }
+//     cin.get(); 
 
-}
+// }
+
+void f(int i,int arr[],int n)
+    {
+        if (i >=(n/2)) return;
+        swap(arr[i],arr[n-i-1]);
+        return f(i+1,arr,n);
+    }
+
+
+int main()
+    {   int n;
+        cin >>n;
+        int arr[n];
+        for (int i = 0;i<n;i++)
+            {
+                cin>>arr[i];
+            }
+        f(0 ,arr,n);
+        for(int i = 0;i<n;i++)
+            cout<< arr[i]<<" ";
+            
+        
+    }
+
--- recurssion.cpp
+++ recurssion.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int sum(int n)
     {
-        if (n == 0) return 0 ;
+        if (n == 0) return 1 ;
 
-        return n + sum(n-1);
+        return (n * sum(n-1));
     }
 
 int main()
